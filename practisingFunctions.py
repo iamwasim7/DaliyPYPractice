cc309fbc01c8b3b0719a9f3288f1920f0ed45089
iamwasim7 <patelwasim884@gmail.com>
2026-09-12 23:28:21 +0530

    Practising Basics of Python.

---
+++ practisingFunctions.py
@@ -0,0 +1,13 @@
+#Python Functions
+#A function is a block of code which only runs when it is called.
+#A function can return data as a result.
+#A function helps avoiding code repetition.
+
+#Creating a Function
+#In Python, a function is defined using the def keyword, followed by a function name and parentheses:
+
+def myfunc():
+    print("I am a Function")
+    
+#Calling a Function
+myfunc()
