0a476bc7108ce71624e5f0eff164fd0943de228e
iamwasim7 <patelwasim884@gmail.com>
2026-09-13 23:18:33 +0530

    range built-in function

---
+++ range.py
@@ -0,0 +1,6 @@
+#Python range
+#The built-in range() function returns an immutable sequence of numbers, commonly used for looping a specific number of times.
+#This set of numbers has its own data type called range.
+x = range(10)
+print(x)
+print(list(x))
