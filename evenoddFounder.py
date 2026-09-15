73480dd60b94f54bf722eb61ede7149601a21244
iamwasim7 <patelwasim884@gmail.com>
2026-09-15 23:15:12 +0530

    EvenOddFounder Progarm

---
+++ evenoddFounder.py
@@ -0,0 +1,7 @@
+def even_odd():
+        num = float(input("Enter a number to find Even/Odd:"))
+        if num % 2 != 0:
+          print("Your number", num, "is ODD")
+        else:
+          print("Your number", num, "is Even")
+even_odd()
