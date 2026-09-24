64e4518355b4c385aef096d2f994d0d99bb26994
iamwasim7 <patelwasim884@gmail.com>
2026-09-24 22:54:29 +0530

    OOP Concept

---
+++ oop.py
@@ -0,0 +1,21 @@
+#Class & Objects in Python.
+
+#creating class
+class Student:
+    name = "Zainab Shaikh"
+
+#creating object (instance)
+s1 = Student()
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
+
+
+class Car:
+    color = "blue"
+    brand = "BMW"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
